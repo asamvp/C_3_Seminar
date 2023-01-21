6523df30989cb9de1bf6a47a5d2af1e7d5d17b6c
Asam <asamvp@mail.ru>
2023-01-21 17:27:56 +0500

    доб задание 4

---
+++ 4/Program.cs
@@ -0,0 +1,20 @@
+﻿/*Напишите программу, которая принимает на вход число (N) и выдаёт таблицу 
+квадратов чисел от 1 до N.
+5 -> 1, 4, 9, 16, 25.
+2 -> 1,4
+*/
+
+int Promt(string message) //ввод значения
+{
+    System.Console.Write($"{message}>");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+Console.Clear();
+int N = Promt("Введите N ");
+int i = 1;
+while (i <= N)
+{
+    System.Console.Write($"{Math.Pow(i, 2)}, ");
+    i++;
+}
