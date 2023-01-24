f35bc96eaaecc3197f6cb4f6ced5ab1c6e4684ad
asam <asamvp@mail.ru>
2023-01-24 10:27:38 +0500

    Доб ДЗ 3

---
+++ Homework_3/Program.cs
@@ -0,0 +1,20 @@
+﻿/*Задача 3
+Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125
+*/
+
+int Promt(string message) //ввод значения
+{
+    System.Console.Write($"{message}>");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+Console.Clear();
+int N = Promt("Введите N ");
+int i = 1;
+while (i <= N)
+{
+    System.Console.Write($"{Math.Pow(i, 3)}, ");
+    i++;
+}
